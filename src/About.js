0b868204e8af8dac022b4a6eecaa09e3b39330d0
Neilketchum <daipayanh@gmail.com>
2020-06-20 20:55:25 +0530

    First Commit

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {BrowserRouter} from "react-router-dom"
+class About extends Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <h1>About</h1>
+            </div>
+           
+            
+        )
+    }
+}
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import About from './About'
+import Contact from './Contact'
+import {Route,Switch,Link} from "react-router-dom"
+import Dog from './Dog'
+import './App.css'
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div className="app-nav">
+          <ul className="nav-elements">
+              <li className = 'ele imp'> <Link to = '/' className = 'ele'>About</Link></li>
+              <li className = 'ele con'>  <Link to = '/Contact' className = 'ele'>Contact</Link></li >
+              <li className = 'ele dog'> <Link to = '/Dog' className = 'ele'>Dog</Link></li>
+          </ul>
+          
+        </div>
+        <Switch>
+          <Route exact  path = '/Contact' component = {Contact}/>
+          <Route exact path = '/' component = {About}/>
+          <Route exact path = '/Dog' component = {Dog}/>
+        </Switch>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
